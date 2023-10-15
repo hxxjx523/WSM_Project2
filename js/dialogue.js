02663dfeda5fe17cb06af2b7c02037eafe77e266
yoonseo232 <ys.bang232@gmail.com>
2023-10-15 21:38:05 +0900

    text 테스트로 넣기

---
+++ js/dialogue.js
@@ -0,0 +1,13 @@
+let dialogue = new Object();
+dialogue.story1=[
+    "오늘은 고등학교 입학식이다. 왜인지 상쾌한 기분! 좋은 일이 있을 것만 같다.",
+    "\" 콰당!! \""
+];
+
+ let charecter = new Object();
+ charecter.story1=[
+    "김여주",
+    "도윤",
+    "백이현",
+    "최재율"
+ ];
