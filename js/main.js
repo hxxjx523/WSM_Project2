94d777acae17af226e0009012432c968a79bbf65
heeju <hhj05230@gmail.com>
2023-10-17 14:54:50 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ function clickHandler(){
     window.location.href = "start.html";
 }
 
+const screenWidth = window.innerWidth;
+const imageSize = 872;
+const remainingWidth = screenWidth - imageSize;
+const container = getElementsByClassName("container");
+container.style.width = remainingWidth + "px";
+container.style.display = "flex";
+container.style.justifyContent = "center";
+container.style.alignItems = "center";
+container.style.height = "100vw";
+
+
 
