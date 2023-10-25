82065fcce7d9ca8b9ecc0fcec6228ed9678def18
yoonseo232 <ys.bang232@gmail.com>
2023-10-25 09:58:55 +0900

    이미지 추가 및 홈화면 앱 아이콘 변경

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,4 @@
-const loading = document.getElementsByClassName("load");
+const loading = document.getElementsByClassName("loading");
 
 window.onload = function(){
     loading_page.style.display = 'none';
