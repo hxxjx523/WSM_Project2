9ee2e48eed247f2638d9eb607d656577ba4db038
yoonseo232 <ys.bang232@gmail.com>
2023-10-29 15:50:59 +0900

    대사 출력

--- js/dialogue.js
+++ js/dialogue.js
@@ -1,13 +1,30 @@
-let dialogue = new Object();
-dialogue.story1=[
-    "오늘은 고등학교 입학식이다. 왜인지 상쾌한 기분! 좋은 일이 있을 것만 같다.",
-    "\" 콰당!! \""
+const dialogues = [
+    { name: "김여주", text: "오늘은 고등학교 입학식이다. 왜인지 상쾌한 기분! 좋은 일이 있을 것만 같다" },
+    { name: "김여주", text: "\" 콰당!! \"" },
 ];
 
- let charecter = new Object();
- charecter.story1=[
-    "김여주",
-    "도윤",
-    "백이현",
-    "최재율"
- ];
+let currentDialogueIndex = 0;
+
+const nameElement = document.querySelector('.name');
+const dialogueElement = document.querySelector('.dialogue');
+const nextButton = document.getElementById('nextButton');
+
+function displayDialogue(index) {
+    if (index >= dialogues.length) {
+        nextButton.disabled = true;
+        return;
+    }
+
+    const dialogue = dialogues[index];
+    nameElement.textContent = dialogue.name;
+    dialogueElement.textContent = dialogue.text;
+}
+
+nextButton.addEventListener('click', () => {
+    currentDialogueIndex++;
+    displayDialogue(currentDialogueIndex);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayDialogue(currentDialogueIndex);
+});
