43d4055a621afac514dc4c487ed8adeaf07577d3
yoonseo232 <ys.bang232@gmail.com>
2023-11-05 14:29:59 +0900

    배경이미지 변경 추가

--- js/BaekLeeHyunRoute_dialogue.js
+++ js/BaekLeeHyunRoute_dialogue.js
@@ -1,18 +1,18 @@
 const dialogues = [
-    { name: "김여주", text: "저건 픽셀이잖아!", img: "images/김여주 표정5.png"},
-    { name: "", text: "앞에 있는 저 이상한 사람은 꽤 여유롭게 교문을 통과하고 있다", img: "images/백이현 ver.1.png" },
-    { name: "김여주", text: "지금 이게 문제가 아니지! 일단 달리고 보자", img: "images/김여주 표정2.png" },
-    { name: "김여주", text: "허억, 허억,", img: "images/김여주 표정1.png" },
-    { name: "", text: "간신히 교문을 통과했다", img: "" },
-    { name: "", text: "숨을 고르고 있는 나를 그 이상한 픽셀이 쳐다보았다", img: "" },
-    { name: "???", text: "저기 괜찮으세요?", img: "images/백이현 ver.1.png" },
-    { name: "", text: "픽셀 인간이 나에게 말을 걸었다..!", img: "" },    
+    { name: "김여주", text: "저건 픽셀이잖아!", img: "images/김여주 표정5.png", background: "images/등굣길 배경.png"},
+    { name: "", text: "앞에 있는 저 이상한 사람은 꽤 여유롭게 교문을 통과하고 있다", img: "images/백이현 ver.1.png", background: "images/등굣길 배경.png" },
+    { name: "김여주", text: "지금 이게 문제가 아니지! 일단 달리고 보자", img: "images/김여주 표정2.png", background: "images/등굣길 배경.png" },
+    { name: "김여주", text: "허억, 허억,", img: "images/김여주 표정1.png", background: "images/학교 배경.png" },
+    { name: "", text: "간신히 교문을 통과했다", img: "", background: "images/학교 배경.png" },
+    { name: "", text: "숨을 고르고 있는 나를 그 이상한 픽셀이 쳐다보았다", img: "", background: "images/학교 배경.png"},
+    { name: "???", text: "저기 괜찮으세요?", img: "images/백이현 ver.1.png", background: "images/학교 배경.png" },
+    { name: "", text: "픽셀 인간이 나에게 말을 걸었다..!", img: "", background: "images/학교 배경.png" },    
     { name: "김여주", text: "아, 네...", img: "images/김여주 표정2.png" },
-    { name: "???", text: "첫날부터 지각할뻔 하다니 저희 둘 다 웃기네요ㅎㅎ", img: "images/백이현 ver.1.png" },
-    { name: "김여주", text: "하하... 그러게요...", img: "images/김여주 표정4.png" },
-    { name: "???", text: "아, 저는 백이현이라고 해요. 그쪽은 신입생이신가요?", img: "images/백이현 ver.1.png" },
-    { name: "김여주", text: "..네! 저는 김여주라고 합니다.. 1학년이에요..", img: "images/김여주 표정4.png" },
-    { name: "백이현", text: "그렇구나, 1학년 교실은 저쪽이에요. 지금 뛰어가면 늦지는 않을 것 같은데?", img: "images/백이현 ver.1.png" },
+    { name: "???", text: "첫날부터 지각할뻔 하다니 저희 둘 다 웃기네요ㅎㅎ", img: "images/백이현 ver.1.png", background: "images/학교 배경.png" },
+    { name: "김여주", text: "하하... 그러게요...", img: "images/김여주 표정4.png", background: "images/학교 배경.png" },
+    { name: "???", text: "아, 저는 백이현이라고 해요. 그쪽은 신입생이신가요?", img: "images/백이현 ver.1.png", background: "images/학교 배경.png" },
+    { name: "김여주", text: "..네! 저는 김여주라고 합니다.. 1학년이에요..", img: "images/김여주 표정4.png", background: "images/학교 배경.png" },
+    { name: "백이현", text: "그렇구나, 1학년 교실은 저쪽이에요. 지금 뛰어가면 늦지는 않을 것 같은데?", img: "images/백이현 ver.1.png", background: "images/학교 배경.png" },
 ];
 
 const backgroundImg = [
@@ -31,7 +31,6 @@ const nextButton = document.getElementById('nextButton');
 const nameWindow = document.querySelector('.nameWindow');
 
 function displayDialogue(index) {
-    document.body.style.backgroundImage = `url('${backgroundImg[1]}')`;
     if (index >= dialogues.length) {
         nextButton.disabled = true;
         return;
@@ -56,7 +55,7 @@ function displayDialogue(index) {
             characterImageElement.style.float = 'right';
             characterImageElement.style.paddingRight = '10vh';
             isImageOnRight = true;
-        }else if (dialogue.img === 'images/백이현 ver.1.png') {
+        } else{
             characterImageElement.style.float = 'left';
             characterImageElement.style.paddingLeft = '10vh';
             isImageOnRight = false;
@@ -64,6 +63,12 @@ function displayDialogue(index) {
         characterImageElement.style.display = isImageVisible ? 'block' : 'none';
     } else {
         characterImageElement.style.display = 'none';
+    }
+
+    if (dialogue.background) {
+        document.body.style.backgroundImage = `url('${dialogue.background}')`;
+    } else {
+        document.body.style.backgroundImage = 'url("기본 배경 이미지 URL")';
     }
 }
 
--- js/DoyoonRoute_dialogue.js
+++ js/DoyoonRoute_dialogue.js
@@ -1,19 +1,14 @@
 const dialogues = [
-    { name: "김여주", text: "저건 픽셀이잖아!", img: "images/김여주 표정5.png"},
-    { name: "", text: "앞에 있는 저 이상한 사람은 꽤 여유롭게 교문을 통과하고 있다", img: "images/도윤 ver.1.png" },
-    { name: "김여주", text: "지금 이게 문제가 아니지! 일단 달리고 보자", img: "images/김여주 표정2.png" },
-    { name: "김여주", text: "허억, 허억,", img: "images/김여주 표정1.png" },
-    { name: "", text: "간신히 교문을 통과했다", img: "" },
-    { name: "", text: "숨을 고르고 있는 나를 그 이상한 픽셀이 쳐다보았다", img: "" },
-    { name: "???", text: "음...", img: "images/도윤 ver.1.png" },
-    { name: "", text: "픽셀 인간은 뚫어져라 나를 쳐다보다가 쌩 가버렸다", img: "" },    
-    { name: "김여주", text: "...?뭐야", img: "images/김여주 표정3.png" },
-    { name: "김여주", text: "아, 나도 서둘러서 가야겠다", img: "images/김여주 표정2.png" },
-];
-
-const backgroundImg = [
-    "images/등굣길 배경.png",
-    "images/학교 배경.png"
+    { name: "김여주", text: "저건 픽셀이잖아!", img: "images/김여주 표정5.png", background: "images/등굣길 배경.png"},
+    { name: "", text: "앞에 있는 저 이상한 사람은 꽤 여유롭게 교문을 통과하고 있다", img: "images/도윤 ver.1.png", background: "images/등굣길 배경.png" },
+    { name: "김여주", text: "지금 이게 문제가 아니지! 일단 달리고 보자", img: "images/김여주 표정2.png", background: "images/등굣길 배경.png" },
+    { name: "김여주", text: "허억, 허억,", img: "images/김여주 표정1.png", background: "images/학교 배경.png" },
+    { name: "", text: "간신히 교문을 통과했다", img: "", background: "images/학교 배경.png" },
+    { name: "", text: "숨을 고르고 있는 나를 그 이상한 픽셀이 쳐다보았다", img: "", background: "images/학교 배경.png" },
+    { name: "???", text: "음...", img: "images/도윤 ver.1.png", background: "images/학교 배경.png" },
+    { name: "", text: "픽셀 인간은 뚫어져라 나를 쳐다보다가 쌩 가버렸다", img: "", background: "images/학교 배경.png" },    
+    { name: "김여주", text: "...?뭐야", img: "images/김여주 표정3.png", background: "images/학교 배경.png" },
+    { name: "김여주", text: "아, 나도 서둘러서 가야겠다", img: "images/김여주 표정2.png", background: "images/학교 배경.png" }
 ];
 
 let currentDialogueIndex = 0;
@@ -26,8 +21,8 @@ const characterImageElement = document.querySelector('.imgCharacter');
 const nextButton = document.getElementById('nextButton');
 const nameWindow = document.querySelector('.nameWindow');
 
+
 function displayDialogue(index) {
-    document.body.style.backgroundImage = `url('${backgroundImg[1]}')`;
     if (index >= dialogues.length) {
         nextButton.disabled = true;
         return;
@@ -52,7 +47,7 @@ function displayDialogue(index) {
             characterImageElement.style.float = 'right';
             characterImageElement.style.paddingRight = '10vh';
             isImageOnRight = true;
-        }else if (dialogue.img === 'images/도윤 ver.1.png') {
+        } else {
             characterImageElement.style.float = 'left';
             characterImageElement.style.paddingLeft = '10vh';
             isImageOnRight = false;
@@ -61,7 +56,14 @@ function displayDialogue(index) {
     } else {
         characterImageElement.style.display = 'none';
     }
+
+    if (dialogue.background) {
+        document.body.style.backgroundImage = `url('${dialogue.background}')`;
+    } else {
+        document.body.style.backgroundImage = 'url("기본 배경 이미지 URL")';
+    }
 }
+
 
 nextButton.addEventListener('click', () => {
     currentDialogueIndex++;
