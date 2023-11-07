347a1c656dfbb7d893168457375e394f1b2dc7f0
heeju <hhj05230@gmail.com>
2023-11-07 18:05:07 +0900

    백이현 루트 추가

---
+++ js/BaekLeeHyunRoute_dialogue2.js
@@ -0,0 +1,73 @@
+const dialogues = [
+    { name: "", text: "이런저런 과거 회상에 빠져있었다", img: "", background: "images/학교 배경.png" },
+    { name: "", text: "(드르륵) 선생으로 보이는 사람이 들어왔다", img: "", background: "images/학교 배경.png" },
+    { name: "담임선생", text: "자~ 안녕하세요 저는 이번 담임을 맡은 ... ", img: "", background: "images/학교 배경.png" },
+    { name: "", text: "이렇게 새학기가 시작되었다", img: "", background: "images/학교 배경.png" }
+];
+
+let currentDialogueIndex = 0;
+let isImageVisible = true; 
+let isImageOnRight = true; 
+
+const nameElement = document.querySelector('.name');
+const dialogueElement = document.querySelector('.dialogue');
+const characterImageElement = document.querySelector('.imgCharacter');
+const nextButton = document.getElementById('nextButton');
+const nameWindow = document.querySelector('.nameWindow');
+const selectContainer = document.querySelector('.selectContainer');
+const select1 = document.querySelector('.select1');
+const select2 = document.querySelector('.select2');
+
+function displayDialogue(index) {
+    if (index >= dialogues.length) {
+        nextButton.disabled = true;
+        return;
+    }
+
+    const dialogue = dialogues[index];
+    nameElement.textContent = dialogue.name;
+    // dialogueElement.textContent = dialogue.text;
+    // 만약 대화의 이름 (dialogue.name)이 빈 문자열이라면 nameWindow를 숨김
+    if (dialogue.name === "") {
+        nameWindow.style.visibility = 'hidden';
+    } else {
+        nameWindow.style.visibility = 'visible';
+    }
+
+    dialogueElement.textContent = dialogue.text;
+    
+    if (dialogue.img) {
+        characterImageElement.src = dialogue.img;
+        // 이미지 주소가 특정 값일 때만 이미지 위치를 변경
+        if (dialogue.img.startsWith('images/김여주')) {
+            characterImageElement.style.float = 'right';
+            nameWindow.style.float = 'left';
+            characterImageElement.style.paddingRight = '10vh';
+            isImageOnRight = true;
+        } else{
+            characterImageElement.style.float = 'left';
+            nameWindow.style.float = 'right';
+            characterImageElement.style.paddingLeft = '10vh';
+            isImageOnRight = false;
+        }
+        characterImageElement.style.display = isImageVisible ? 'block' : 'none';
+    } else {
+        characterImageElement.style.display = 'none';
+    }
+    
+    if (dialogue.background) {
+        document.body.style.backgroundImage = `url('${dialogue.background}')`;
+    } else {
+        document.body.style.backgroundImage = 'url("기본 배경 이미지 URL")';
+    }
+}
+
+nextButton.addEventListener('click', () => {
+    currentDialogueIndex++;
+    displayDialogue(currentDialogueIndex);
+    console.log(currentDialogueIndex);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayDialogue(currentDialogueIndex);
+});
