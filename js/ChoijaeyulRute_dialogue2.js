b6b80d521e25a07e28090d8d5191e2b9753ffe85
yoonseo232 <ys.bang232@gmail.com>
2023-11-07 19:15:18 +0900

    대사 추가

--- js/ChoijaeyulRute_dialogue2.js
+++ js/ChoijaeyulRute_dialogue2.js
@@ -6,7 +6,26 @@ const dialogues = [
     { name: "???", text: "같은 반 이었네, 최재율이라고 해", img: "images/최재율 ver.1-2.png", background: "images/교실 배경.png" },
     { name: "최재율", text: "잘 부탁해", img: "images/최재율 ver.1-2.png", background: "images/교실 배경.png" },
     { name: "", text: "그의 픽셀 미소는 살인적이다 못해 웃겼다", img: "", background: "images/교실 배경.png"},
-    { name: "김여주", text: "..안녕!", img: "images/김여주 표정4.png", background: "images/교실 배경.png" },
+    { name: "김여주", text: "푸훕..", img: "images/김여주 표정8.png", background: "images/교실 배경.png" },
+    { name: "", text: "그만 웃어버리고 말았다", img: "", background: "images/교실 배경.png"},
+    { name: "최재율", text: "응? 뭐야 왜 웃는 거야?", img: "images/최재율 ver.1.png", background: "images/교실 배경.png" },
+    { name: "김여주", text: "아.. 진짜 환하게 웃는 것 같아서..", img: "images/김여주 표정4.png", background: "images/교실 배경.png" },
+    { name: "최재율", text: "..그래? 고마워", img: "images/최재율 ver.1.png", background: "images/교실 배경.png" },
+    { name: "최재율", text: "근데 너는 이 학교에 어떻게 왔어?", img: "images/최재율 ver.1.png", background: "images/교실 배경.png" },
+    { name: "김여주", text: "그게..", img: "images/김여주 표정4.png", background: "images/교실 배경.png" },
+];
+
+const select1_dialogues = [  
+    { name: "김여주", text: "그냥 여러가지.. ", img: "images/김여주 표정5.png", background: "images/교실 배경.png" },
+    { name: "최재율", text: "그래? 더 물어보면 실례겠네", img: "images/최재율 ver.1.png", background: "images/교실 배경.png" },
+    { name: "김여주", text: "어? 그렇게까ㅈ", img: "images/김여주 표정4.png", background: "images/교실 배경.png" },
+    { name: "", text: "(드르륵)", img: "", background: "images/교실 배경.png"},
+    { name: "", text: "선생으로 보이는 사람이 들어왔다", img: "", background: "images/교실 배경.png"},
+];
+
+const select2_dialogues = [
+    { name: "김여주", text: "미남이 많아서! 난 잘생긴 사람 진짜 좋아하거든~", img: "images/김여주 표정7.png", background: "images/교실 배경.png" },      
+    { name: "김여주", text: "내가 처음보는 사람한테 무슨 말을..", img: "images/김여주 표정1.png", background: "images/교실 배경.png" },      
 ];
 
 let currentDialogueIndex = 0;
@@ -18,6 +37,9 @@ const dialogueElement = document.querySelector('.dialogue');
 const characterImageElement = document.querySelector('.imgCharacter');
 const nextButton = document.getElementById('nextButton');
 const nameWindow = document.querySelector('.nameWindow');
+const selectContainer = document.querySelector('.selectContainer');
+const select1 = document.querySelector('.select1');
+const select2 = document.querySelector('.select2');
 
 
 function displayDialogue(index) {
@@ -28,7 +50,7 @@ function displayDialogue(index) {
 
     const dialogue = dialogues[index];
     nameElement.textContent = dialogue.name;
-
+    // dialogueElement.textContent = dialogue.text;
     // 만약 대화의 이름 (dialogue.name)이 빈 문자열이라면 nameWindow를 숨김
     if (dialogue.name === "") {
         nameWindow.style.visibility = 'hidden';
@@ -37,7 +59,7 @@ function displayDialogue(index) {
     }
 
     dialogueElement.textContent = dialogue.text;
-
+    
     if (dialogue.img) {
         characterImageElement.src = dialogue.img;
         // 이미지 주소가 특정 값일 때만 이미지 위치를 변경
@@ -64,17 +86,47 @@ function displayDialogue(index) {
     }
 }
 
+function yeojooChoise(index){
+    
+    let likability = 0;
+    let currentSelect1Index = 0;
+
+
+    const dialogue = select1_dialogues[0];
+    
+    select1.addEventListener('click', () => {
+        selectContainer.style.display = 'none';
+        likability++;
+        console.log(likability);
+        dialogueElement.textContent = dialogue.text;
+        if (dialogue.name === "" && dialogue.img === "") {
+            nameWindow.style.visibility = 'hidden';
+            characterImageElement.style.visibility = 'hidden';
+        }
+            
+    });
+        
+        const dialogue2 = select2_dialogues[0];
+        select2.addEventListener('click', () => {
+            selectContainer.style.display = 'none';
+            dialogueElement.textContent = dialogue2.text;
+            if (dialogue2.name === "" && dialogue2.img === "") {
+                nameWindow.style.visibility = 'hidden';
+                characterImageElement.style.visibility = 'hidden';
+            }
+        });
+    }
 
 nextButton.addEventListener('click', () => {
     currentDialogueIndex++;
-    displayDialogue(currentDialogueIndex);
-    console.log(currentDialogueIndex)
+        if(currentDialogueIndex === 13){
+            selectContainer.style.display = 'flex';
+            yeojooChoise(currentDialogueIndex);
+        }
+        displayDialogue(currentDialogueIndex);
+        console.log(currentDialogueIndex);
 });
 
 window.addEventListener('DOMContentLoaded', () => {
     displayDialogue(currentDialogueIndex);
 });
-
-// if(currentDialogueIndex === 16){
-//     window.location.href = mainBoy + "Route.html";
-// }
